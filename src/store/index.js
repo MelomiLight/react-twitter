8743797dfcd5a0597d42283ec6f3596f38d2f999
Denys Dovhan <denysdovhan@gmail.com>
2018-08-04 18:37:30 +0300

    Add persistence for store

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import enhance from './enhance';
 import middlewares from './middlewares';
 import rootReducer from '../modules';
+import { saveStore, loadStore } from '../utils/storage';
 
-export default function configureStore(initialState = {}) {
+export default function configureStore(initialState = loadStore()) {
   const enhancers = enhance(applyMiddleware(...middlewares));
   const store = createStore(rootReducer, initialState, enhancers);
 
@@ -12,6 +13,10 @@ export default function configureStore(initialState = {}) {
       store.replaceReducer(rootReducer);
     });
   }
+
+  store.subscribe(() => {
+    saveStore(store.getState());
+  });
 
   return store;
 }
