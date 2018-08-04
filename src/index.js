678dd08b023be2a9b9afabb17f784f99059c1f31
Denys Dovhan <denysdovhan@gmail.com>
2018-08-04 17:48:27 +0300

    Add basics for React app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Root from './containers/Root';
+import configureStore from './store';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore();
+
+const render = Component => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.getElementById('root'),
+  );
+};
+
+render(Root);
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => {
+    render(Root);
+  });
+}
 
 registerServiceWorker();
